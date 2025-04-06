3c94587e2cc0f42ce3f3d71f20b41e8aebff1789
Jacob Malland <jacob.malland@gmail.com>
2025-04-06 11:13:15 -0500

    #1 Created initial (unedited) code, to reference with code after completion.

---
+++ initial code/Process.java
@@ -0,0 +1,14 @@
+
+
+public class Process {
+	public int pid, arrivalTime, serviceTime, burstTime, responseTime, completionTime;
+
+	public Process(int pid, int arrivalTime, int burstTime) {
+		this.pid = pid; // unique
+		this.arrivalTime = arrivalTime;
+		this.burstTime = burstTime;
+		this.serviceTime = 0;
+		this.responseTime = -1;
+	}
+
+}
